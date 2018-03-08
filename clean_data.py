c0d79687ef9d80bf304a63bdd345fe0497d829a7
Lucy Lee <lucylee210@gmail.com>
2018-03-08 13:41:04 -0500

    Cleaned create_year_field function

--- clean_data.py
+++ clean_data.py
@@ -4,7 +4,6 @@
 # Code to clean MassGIS OpenSpace shapefile
 
 import sys
-from datetime import date
 try:
     import arcpy
 except ImportError:
@@ -36,70 +35,75 @@ def del_fields(shp, keep_fields):
 
 
 ##########
+# create_str_date
+#
+# creates a text field that is a copy of the date field (MM/DD/YYYY).
+# this is done in order to slice the string date to create a year field.
+# datetime methods for string formatting do not cover years prior to 1900,
+# which are present in the MassGIS shapefile, so this method is done instead.
+##########
+
+def create_str_date(in_table, date_field):
+    new_field = "STR_DATE"
+    try:
+        arcpy.AddField_management(in_table, new_field, "TEXT")
+        with arcpy.da.UpdateCursor(in_table, [new_field, date_field]) as cursor:
+            for row in cursor:
+                if row[1] is not None:
+                    row[0] = row[1]
+                else:
+                    row[0] = 0   # If unknown set to 0 because field is not nullable
+                cursor.updateRow(row)
+        del row
+        del cursor
+    except Exception:
+        pass
+
+
+##########
 # create_year_field
 #
 # creates a year field from existing date field
+# call create_str_date and deletes the new string date field
 # new field is short integer
 ##########
 
 def create_year_field(in_table, year_field, date_field):
     
-    # Check to see if the new field to create already exists
+    # Create string copy of date field and new integer year field
     try:
+        create_str_date(in_table, date_field)
         arcpy.AddField_management(in_table, year_field, "SHORT")
     except Exception:
-        # If the field exists, ask user if they wish to overwrite
-        q = int(raw_input("Field " + year_field + " already exists. Overwrite years?" + \
-                      "\nEnter 1 for yes and 0 for no:   "))
-        
-    # If the user wants to overwrite years, do that    
-    if q == 1:
-        try:
-            # Make a string copy of the date field to enable string slicing
-            arcpy.AddField_management(in_table, "STR_DATE", "TEXT")
-            with arcpy.da.UpdateCursor(in_table, ["STR_DATE", date_field]) as cursor:
-                for row in cursor:
-                    if row[1] is not None:
-                        row[0] = row[1]
-                    else:
-                        row[0] = 0    # If unknown set to zero because shp is not nullable
-                    cursor.updateRow(row)
-        except Exception:
-            pass
+        pass
 
-        # Create the year field using the string 
-        fields = [year_field, "STR_DATE"]
+    # Create the year field using the string date field
+    str_date_field = "STR_DATE"
+    fields = [year_field, str_date_field]
 
-        with arcpy.da.UpdateCursor(in_table, fields) as cursor:
-            for row in cursor:
-                if row[1] != "0":
-                    year = row[1][-4:]
-                    row[0] = int(year)
-                else:
-                    row[0] = row[1]    # Set year to 0 if unknown
-                cursor.updateRow(row)
-        del row
-        del cursor
-        try:
-            arcpy.DeleteField_management(in_table, "STR_DATE")
-        except Exception:
-            pass
-        print 'Done!'
-    # Otherwise, exit 
-    elif q == 0:
-        sys.exit()
-    else:
-        print "Not a recognized response. Exiting function."
-        sys.exit()
-    
+    # Update year field with year or 0 if unknown
+    with arcpy.da.UpdateCursor(in_table, fields) as cursor:
+        for row in cursor:
+            if row[1] != "0":
+                row[0] = row[1][-4:]
+            else:
+                row[0] = row[1]
+            cursor.updateRow(row)
+    del row
+    del cursor
 
-shp = "OPENSPACE_POLY.shp"
-create_year_field(shp, "YEAR", "CAL_DATE_R")
+    # Delete the string date field to keep attribute table clean
+    arcpy.DeleteField_management(in_table, str_date_field)
 
-    
-## CAL_DATE_R
+    print "Done! Short integer field", year_field, "created in", in_table
+
+
 
 # Trim the openspace attribute table to retain only most relevant fields
 ##keep_fields = ["FID", "Shape", "TOWN_ID", "SITE_NAME", "FEE_OWNER", "PUB_ACCESS", \
 ##               "LEV_PROT", "GIS_ACRES", "CAL_DATE_R"]
 ##del_fields(shp, keep_fields)
+
+# Create year field
+shp = "OPENSPACE_POLY.shp"
+create_year_field(shp, "YEAR", "CAL_DATE_R")
